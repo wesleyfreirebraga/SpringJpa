5ae277757d5d49591d65bbaa4a32670446b6e0ee
wesley braga <wesley.freire@el.com.br>
2023-08-09 12:29:09 -0300

    restore

--- src/main/java/com/AulaJava/curso/Resources/UserResource.java
+++ src/main/java/com/AulaJava/curso/Resources/UserResource.java
@@ -1,10 +1,18 @@
 package com.AulaJava.curso.Resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value="users")
-public class UserResource {
+import com.AulaJava.curso.Entities.User;
 
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	@GetMapping
+ public ResponseEntity<User> findAll(){
+	return null;
+	 
+ };
 }
